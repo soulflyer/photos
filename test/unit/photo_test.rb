354d25cb71b1161f082819fb4c9da3a8fa4d4dab
Iain <public@soulflyer.co.uk>
2012-09-19 14:01:17 +0700

    added some tests for Photo

--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -4,4 +4,13 @@ class PhotoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "must have at least a fullsize entry" do
+    photo = Photo.new
+    assert photo.invalid?
+    assert photo.errors[:fullsize].any?
+    assert photo.errors[:shoot_id].any?
+    photo.fullsize = "/tmp/pic.jpg"
+    photo.shoot_id = 1
+    assert photo.valid?
+  end
 end
